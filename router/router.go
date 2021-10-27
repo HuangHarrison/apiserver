8ae770314016575ef5db6416a2f8ac71b252c819
Harrison <huangharrsion@gmail.com>
2021-10-27 22:37:13 +0800

    基础 2：配置文件读取

--- router/router.go
+++ router/router.go
@@ -8,14 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Load(g *gin.Engine, mn ...gin.HandlerFunc) *gin.Engine {
+// Load loads the middlewares, routes, handlers.
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	// Middlewares.
 	g.Use(gin.Recovery())
 	g.Use(middleware.NoCache)
 	g.Use(middleware.Options)
 	g.Use(middleware.Secure)
-	g.Use(mn...)
-	// 404 Handler
+	g.Use(mw...)
+	// 404 Handler.
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
@@ -25,7 +26,7 @@ func Load(g *gin.Engine, mn ...gin.HandlerFunc) *gin.Engine {
 	{
 		svcd.GET("/health", sd.HealthCheck)
 		svcd.GET("/disk", sd.DiskCheck)
-		svcd.GET("cpu", sd.CPUCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
 		svcd.GET("/ram", sd.RAMCheck)
 	}
 
