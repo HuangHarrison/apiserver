fed3a29cf5af41cdef17dc2acb2614e86d58d84d
Harrison <huangharrsion@gmail.com>
2021-11-14 17:08:40 +0800

    基础 7：读取和返回 HTTP 请求

--- handler/user/create.go
+++ handler/user/create.go
@@ -2,51 +2,48 @@ package user
 
 import (
 	"fmt"
-	"net/http"
 
+	. "apiserver/handler"
 	"apiserver/pkg/errno"
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
 )
 
+// Create creates a new user account.
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-
-	var err error
+	var r CreateRequest
+	// 检查 Content-Type 类型，将消息体作为指定的格式解析到 Go struct 变量中
 	if err := c.Bind(&r); err != nil {
-		c.JSON(
-			http.StatusOK,
-			gin.H{"error": errno.ErrBind},
-		)
+		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
-	log.Debugf("username is: [%s], passwird is [%s]", r.Username, r.Password)
-	if r.Username == "" {
-		err = errno.New(
-			errno.ErrUserNotFound,
-			fmt.Errorf("username can not found in db: xx.xx.xx.xx"),
-		).Add("This is add message.")
-		log.Errorf(err, "Get an error")
-	}
+	// 返回 URL 的参数值
+	admin2 := c.Param("username")
+	log.Infof("URL username: %s", admin2)
 
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
+	// 读取 URL 中的地址参数
+	desc := c.Query("desc")
+	log.Infof("URL key param desc: %s", desc)
+
+	// 获取 HTTP 头
+	contentType := c.GetHeader("Content-Type")
+	log.Infof("Header Content-Type: %s", contentType)
+
+	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
+	if r.Username == "" {
+		SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+		return
 	}
 
 	if r.Password == "" {
-		err = fmt.Errorf("password is empty")
+		SendResponse(c, fmt.Errorf("password is empty"), nil)
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(
-		http.StatusOK,
-		gin.H{
-			"code":    code,
-			"message": message,
-		},
-	)
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+
+	// Show the user information.
+	SendResponse(c, nil, rsp)
 }
